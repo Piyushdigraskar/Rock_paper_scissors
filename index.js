e4c634f2b120496b5342be4305b676468b9f73ed
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-12-15 15:34:59 +0530

    made buttons and moved all logic from console to screen

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
+const container = document.querySelector('.container');
+const rockbtn = document.createElement('button')
+const paperbtn = document.createElement('button')
+const scissorsbtn = document.createElement('button');
+const humanscorebtn = document.createElement('button');
+const compscorebtn = document.createElement('button');
+
+
+rockbtn.style.backgroundColor = "green";
+paperbtn.style.backgroundColor = "green";
+scissorsbtn.style.backgroundColor = "green";
+humanscorebtn.style.backgroundColor = "yellow";
+compscorebtn.style.backgroundColor = "yellow";
+
+rockbtn.style.color = "white";
+paperbtn.style.color = "white";
+scissorsbtn.style.color = "white";
+humanscorebtn.style.color = "black";
+compscorebtn.style.color = "black";
+
+rockbtn.textContent = "Rock";
+paperbtn.textContent = "Paper";
+scissorsbtn.textContent = "Scissors";
+container.appendChild(rockbtn);
+container.appendChild(paperbtn);
+container.appendChild(scissorsbtn);
+container.appendChild(humanscorebtn);
+container.appendChild(compscorebtn);
+const resultsDiv = document.createElement('div');
+container.appendChild(resultsDiv);
+
+
 let humanScore = 0;
 let computerScore = 0;
+humanscorebtn.textContent = `Human: ${humanScore}`;
+compscorebtn.textContent = `Computer: ${computerScore}`;
 
 function getComputerChoice() {
 
@@ -15,56 +49,45 @@ function getComputerChoice() {
        }
 }
 
-function getHumanChoices() {
-       let humanChoice = prompt("Give one between rock, paper and scissors:");
-       return humanChoice.toLowerCase();
-}
-
-
-function playRound(humanChoice, computerChoise) {
-       if (humanChoice === "rock" && computerChoise === "paper") {
-              computerScore++;
-              console.log("you lose, paper beats rock");
-       } else if (humanChoice === "rock" && computerChoise === "scissors") {
-              humanScore++;
-              console.log("you win, rock beats scissors");
-       } else if (humanChoice === "paper" && computerChoise === "scissors") {
-              computerScore++;
-              console.log("you lose, scissors beats paper");
-       } else if (humanChoice === "paper" && computerChoise === "rock") {
-              humanScore++;
-              console.log("you win, paper beats rock");
-       } else if (humanChoice === "scissors" && computerChoise === "rock") {
-              computerScore++;
-              console.log("you lose, rock beats scissors");
-       } else if (humanChoice === "scissors" && computerChoise === "paper") {
-              humanScore++;
-              console.log("you win, scissors beats paper");
+function handleRound(humanChoice) {
+       let computerChoice = getComputerChoice();
+       if (
+           (humanChoice === "rock" && computerChoice === "scissors") ||
+           (humanChoice === "paper" && computerChoice === "rock") ||
+           (humanChoice === "scissors" && computerChoice === "paper")
+       ) {
+           humanScore++;
+           resultsDiv.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+       } else if (humanChoice === computerChoice) {
+           resultsDiv.textContent = "It's a draw!";
        } else {
-              humanScore++;
-              computerScore++;
-              console.log("Both are same, its a draw");
+           computerScore++;
+           resultsDiv.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
        }
-}
+       humanscorebtn.textContent = `Human: ${humanScore}`;
+       compscorebtn.textContent = `Computer: ${computerScore}`;
 
-function playGame() {
-       let repeat = 5;
-       while (repeat > 0) {
-              let humanChoice = getHumanChoices();
-              let computerChoise = getComputerChoice();
-              playRound(humanChoice, computerChoise);
-              repeat--;
+       if(humanScore === 5 || computerScore === 5){
+              if(humanScore === 5){
+                     resultsDiv.textContent ='Yeah, the user wins';
+              }
+              else{
+                     resultsDiv.textContent ='ohh, Computer wins';
+              }
+              
+              resetGame();
        }
-       if (humanScore > computerScore) {
-              console.log("Congratulations, You won the tournament");
-       } else if (humanScore < computerScore) {
-              console.log("Better luck next time, You lost the tournament");
-       } else {
-              console.log("The tournament was a Draw");
-       }
-}
-playGame();
+   }
 
-
-
-
+   function resetGame() {
+       humanScore = 0;
+       computerScore = 0;
+       humanscorebtn.textContent = `Human: ${humanScore}`;
+       compscorebtn.textContent = `Computer: ${computerScore}`;
+       resultsDiv.textContent ="Game has been reset!";
+   }
+   
+   // Event Listeners
+   rockbtn.addEventListener('click', () => handleRound("rock"));
+   paperbtn.addEventListener('click', () => handleRound("paper"));
+   scissorsbtn.addEventListener('click', () => handleRound("scissors"));
